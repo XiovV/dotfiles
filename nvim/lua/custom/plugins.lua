f1a7cdc7bc4bff290fa4b84ed1104e95ef524875
Damir H <xiovv@tutanota.com>
2023-07-12 10:58:40 +0200

    nvim lua

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,37 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "andweeb/presence.nvim",
+    lazy = false,
+  }
+}
+
+return plugins
