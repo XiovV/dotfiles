aaf676440ac1782b8733ece189ebe832bf0dc74f
Damir H <xiovv@tutanota.com>
2023-12-08 17:36:53 +0100

    add configuration for JS and TS

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,52 @@
+local plugins = {
+    {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python", "go", "js", "ts", "jsx", "tsx"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "eslint-lsp",
+        "typescript-language-server",
+        "gopls",
+        "goimports",
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "andweeb/presence.nvim",
+    lazy = false,
+  }
+}
+
+return plugins
